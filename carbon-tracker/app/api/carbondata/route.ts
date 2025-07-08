eeb0eed245ad2eb1c1b687755ed874974a116aec
John J Eze <johneze.webdev03@gmail.com>
2025-07-08 09:58:44 +0000

    Got carbon data api to work: Next fixing it up

--- carbon-tracker/app/api/carbondata/route.ts
+++ carbon-tracker/app/api/carbondata/route.ts
@@ -1,6 +1,6 @@
-import { carbonData } from "@/app/carbon-data"
+import { carbonData } from "../../carbon-data"
 
-export default async function GET(){
+export async function GET(){
   
    return new Response (JSON.stringify(carbonData), {
         status: 200,
@@ -10,3 +10,4 @@ export default async function GET(){
    })
   
 };
+
--- carbon-tracker/app/page.tsx
+++ carbon-tracker/app/page.tsx
@@ -1,11 +1,22 @@
 import Image from "next/image";
 
+export default async function Home() {
+  const carbonData =  await fetch('https://effective-cod-vwp7vvw5r47266pw.github.dev/api/carbondata', {cache: 'no-cache'})
+  
+   if (!carbonData.ok) {
+    const errorText = await carbonData.text(); // grab HTML or error message
+    console.error('Fetch failed:', errorText);
+    throw new Error(`Failed to fetch carbon data: ${carbonData.status}`);
+  }
 
+  const carbonRes = await carbonData.json()
 
-export default function Home() {
   return (
     <>
       <h1 className="text-2xl text-center font-semibold text-cyan-300">Carbon Footrpint Tracker</h1>
+      <div>
+        {carbonRes}
+      </div>
     </>
   );
 }
