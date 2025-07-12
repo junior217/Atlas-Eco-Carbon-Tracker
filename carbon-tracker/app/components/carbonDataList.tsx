7d4452fcb1742fbc38cd191059f5d4321f452818
John J Eze <johneze.webdev03@gmail.com>
2025-07-12 08:09:45 +0000

    Had to fix package.json so that i can run developement environment

--- carbon-tracker/app/components/carbonDataList.tsx
+++ carbon-tracker/app/components/carbonDataList.tsx
@@ -1,21 +1,41 @@
 import { use } from "react"
 import { CarbonData, getSectorData } from "../lib/utils/getCarbonData"
 
+const getActivities = (sectorIndex: number) => {
+    const carbonList = use(CarbonData())
+    const currentYearList = carbonList[0]
+    const sectorList = getSectorData(sectorIndex)
+
+    if (!sectorList || !sectorList[0]) {
+        return [];
+    }
+
+    var activities = [];
+
+    for(let i = 0; i > sectorList[sectorIndex].length; i++){
+      const activity = sectorList[sectorIndex].activties[i] 
+      activities.push(activity) 
+    }
+
+    return activities;
+}
+
 export default function CarbonDataList(){
     const carbonList = use(CarbonData())
     const currentYearList = carbonList[0]
 
     const sectorList = getSectorData(0)
-    console.log(sectorList)
+    const activitiesList = getActivities(0)
+    console.log(activitiesList)
     
 
     console.log(typeof(carbonList))
     console.log(currentYearList)
 
     return (
-      <div>
-        {carbonList.map((data: any, index: number) => (<div key={index}></div>))}
-      </div>
+       <>
+        <h1 className="text-2xl">Hi</h1>
+       </> 
     );
 
 }
