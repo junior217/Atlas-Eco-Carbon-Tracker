2a4c5c44dd2c0b3e551a4022ad38dbdf7a05b530
John J Eze <johneze.webdev03@gmail.com>
2025-07-14 10:54:57 +0200

    Update page.tsx
    Finished Up abit of the Home Page

--- carbon-tracker/app/page.tsx
+++ carbon-tracker/app/page.tsx
@@ -1,33 +1,120 @@
 import Image from "next/image";
+import Link from "next/link";
 //import CarbonDataList from "./components/carbonDataList";
 
 export default async function Home() {
   return (
-    <>
-      <h1 className="text-2xl font-semibold text-cyan-400 text-center">
-        Carbon Tracker Home Page
-      </h1>
-      <div className="flex flex-col items-center justify-center mt-10 md:flex-row md:space-x-10">
-        <div>
-          <button className="btn btn-primary w-1/2 h-10">Button</button>
-        </div>
-        <form action="submit">
-          <div className="form-control w-60 mb-4">
-            <label className="label floating-label">
-              <span className="label-text">Enter your carbon data</span>
-            </label>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 p-4 ">
+      
+        <Image
+          src="/public/logo.png"
+          alt="logoimage"
+          className="object-cover"
+          width={20}
+          height={20}
+        ></Image>
+      
+
+      <div className="flex flex-col items-center justify-center w-full max-w-md h-[100%] bg-gray-100 rounded-lg shadow-lg p-6 mt-10 ">
+        <Image
+          src="/"
+          alt="logoimage"
+          className="object-cover justify-center"
+          width={20}
+          height={20}
+        ></Image>
+
+        <h1 className="text-4xl font-bold text-center mb-6">Carbon Tracker</h1>
+
+        <form action="" className="flex flex-col items-center space-y-4 p-5">
+          <button className="btn bg-white text-black border-[#e5e5e5]">
+            <svg
+              aria-label="Google logo"
+              width="16"
+              height="16"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <g>
+                <path d="m0 0H512V512H0" fill="#fff"></path>
+                <path
+                  fill="#34a853"
+                  d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                ></path>
+                <path
+                  fill="#4285f4"
+                  d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                ></path>
+                <path
+                  fill="#fbbc02"
+                  d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                ></path>
+                <path
+                  fill="#ea4335"
+                  d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                ></path>
+              </g>
+            </svg>
+            Login with Google
+          </button>
+
+          <p className="text-gray-500">or</p>
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              </g>
+            </svg>
+            <input type="email" placeholder="mail@site.com" required />
+          </label>
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
             <input
-              type="text"
-              placeholder="Type here"
-              className="input-ghost input-bordered w-full input-success"
+              type="password"
+              required
+              placeholder="Password"
+              minLength="8"
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
             />
-          </div>
-          <button type="submit" className="btn btn-primary w-60 h-10">
-            Submit
+          </label>
+          <button type="submit" className="btn btn-primary my-2 p-5">
+            Log In
           </button>
         </form>
-  
+
+        <p className="text-xs text-center py-5 sm:text-base sm:text-left">By continuing, you agree to our Terms and Privacy Policy.</p>
+        <Link href="/signUp" className="text-blue-500 hover:underline text-xs justify-center text-center">
+          Don't have an account? Sign Up
+        </Link>
       </div>
-    </>
+    </div>
+    // cabron List Claro 
   );
 }
