583a9cab8699b8caf6118e0d0613221917d4ad05
John J Eze <johneze.webdev03@gmail.com>
2025-07-16 08:11:13 +0000

    Homepage Begun Background Set

--- carbon-tracker/app/page.tsx
+++ carbon-tracker/app/page.tsx
@@ -4,10 +4,8 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
+    <div className="bg-home flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+      
+    </div>
   );
 }
