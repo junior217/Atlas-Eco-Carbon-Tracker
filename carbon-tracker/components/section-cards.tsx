f598a785188a01a8e0cdc1556a34d8f20279c6b8
John J Eze <johneze.webdev03@gmail.com>
2025-09-09 14:57:17 +0000

    Dashboard Begun

--- carbon-tracker/components/section-cards.tsx
+++ carbon-tracker/components/section-cards.tsx
@@ -1,102 +1,109 @@
-import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react"
+import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react";
 
-import { Badge } from "@/components/ui/badge"
+import { Badge } from "@/components/ui/badge";
 import {
   Card,
   CardAction,
+  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
 export function SectionCards() {
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>Total Revenue</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            $1,250.00
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +12.5%
+          <CardTitle className="text-2xl font-bold">Energy</CardTitle>
+          <CardDescription className="mt-2 flex items-center gap-2 text-3xl font-bold">
+            14,000 MtCO2
+            <Badge variant="secondary" className="rounded-md px-2 py-1 text-sm">
+              +5.2%
             </Badge>
-          </CardAction>
+          </CardDescription>
         </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-sm text-muted-foreground">
+              Activities:{" "}
+              <span className="font-slate-400 text-xs">Emissions_Mtco2</span>
+            </h1>
+            <ul>
+              <li className="flex justify-between text-sm">
+                <span>Electricity & Heat Production</span>
+                <span>9,200 </span>
+              </li>
+              <li className="flex justify-between text-sm">
+                <span>Manufacturing & Construction</span>
+                <span>2,800 </span>
+              </li>
+              <li className="flex justify-between text-sm">
+                <span>Fugitive Emissions</span>
+                <span>2,000 </span>
+              </li>
+            </ul>
+          </div>
+        </CardContent>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <IconTrendingUp className="size-4" />
+            Global Yearly Change <IconTrendingDown className="size-4" />
           </div>
-          <div className="text-muted-foreground">
-            Visitors for the last 6 months
-          </div>
+          <div className="text-muted-foreground">1.77%</div>
         </CardFooter>
       </Card>
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>New Customers</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,234
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingDown />
-              -20%
+          <CardTitle className="text-2xl font-bold">Transportation</CardTitle>
+          <CardDescription className="mt-2 flex items-center gap-2 text-3xl font-bold">
+            7200 MtCO2
+            <Badge variant="secondary" className="rounded-md px-2 py-1 text-sm">
+              +5.2%
             </Badge>
-          </CardAction>
+          </CardDescription>
         </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-sm text-muted-foreground">
+              Activities:{" "}
+              <span className="font-slate-400 text-xs">Emissions_Mtco2</span>
+            </h1>
+            <ul>
+              <li className="flex justify-between text-sm">
+                <span>Passenger Cars</span>
+                <span>3400</span>
+              </li>
+              <li className="flex justify-between text-sm">
+                <span>Heavy Trucks</span>
+                <span>1200 </span>
+              </li>
+              <li className="flex justify-between text-sm">
+                <span>Light Commercial Vehicles</span>
+                <span>700</span>
+              </li>
+              <li className="flex justify-between text-sm">
+                <span>Aviation</span>
+                <span>1100</span>
+              </li>
+              <li className="flex justify-between text-sm">
+                <span>Shipping</span>
+                <span>600</span>
+              </li>
+              <li className="flex justify-between text-sm">
+                <span>Rail</span>
+                <span>100 </span>
+              </li>
+            </ul>
+          </div>
+        </CardContent>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
           <div className="line-clamp-1 flex gap-2 font-medium">
-            Down 20% this period <IconTrendingDown className="size-4" />
+            Global Yearly Change <IconTrendingDown className="size-4" />
           </div>
-          <div className="text-muted-foreground">
-            Acquisition needs attention
-          </div>
-        </CardFooter>
-      </Card>
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Active Accounts</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            45,678
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +12.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Strong user retention <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Engagement exceed targets</div>
-        </CardFooter>
-      </Card>
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Growth Rate</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            4.5%
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +4.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Steady performance increase <IconTrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Meets growth projections</div>
+          <div className="text-muted-foreground">3.77%</div>
         </CardFooter>
       </Card>
     </div>
-  )
+  );
 }
